af7ebb16d2f6444d6c7d78db592673070f2b2b07
def rijay <defrijay@gmail.com>
2024-06-07 17:47:38 +0700

    tambah folder

---
+++ database/migrations/2024_06_05_041518_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('status');
+            $table->string('priority');
+            $table->string('due_date')->nullable();
+            $table->string('assigned_user_id')->constrained('users');
+            $table->string('created_by')->constrained('users');
+            $table->string('updated_by')->constrained('users');
+            $table->string('task_id')->constrained('tasks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
